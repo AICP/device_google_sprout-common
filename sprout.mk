37c48d16cbbebeb6464bdca9526742802970d399
Varun Chitre <varun.chitre15@gmail.com>
2015-06-19 05:57:04 -0400

    sprout-common : cleanup
    Change-Id: Id3872521ca07e4058da03bd7bacee7762b7f1146

--- sprout.mk
+++ sprout.mk
@@ -129,15 +129,13 @@ PRODUCT_PACKAGES += \
     setup_fs \
     e2fsck \
 
-# Delegation for OEM customization
+# Dynamically set props
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
     ro.product.name \
     ro.product.manufacturer
 
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-PRODUCT_PACKAGES += Torch
 
 # call the proprietary setup
 $(call inherit-product, vendor/google/sprout/sprout-vendor.mk)
